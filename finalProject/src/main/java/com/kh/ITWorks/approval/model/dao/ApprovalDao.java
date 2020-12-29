666aca1d0a4e1efaba46ce660ae8ad852f467cb8
seodj93 <seodj93@naver.com>
2020-12-30 00:19:21 +0900

    Revert "Revert "update approval""
    This reverts commit 6670b20a4991c68eb99a8ddb1462296e968db812.

--- finalProject/src/main/java/com/kh/ITWorks/approval/model/dao/ApprovalDao.java
+++ finalProject/src/main/java/com/kh/ITWorks/approval/model/dao/ApprovalDao.java
@@ -1,5 +1,45 @@
 package com.kh.ITWorks.approval.model.dao;
 
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.kh.ITWorks.approval.model.vo.ApprovalDocument;
+import com.kh.ITWorks.approval.model.vo.ApprovalLine;
+import com.kh.ITWorks.approval.model.vo.Attachment;
+import com.kh.ITWorks.approval.model.vo.DocumentsRequest;
+import com.kh.ITWorks.approval.model.vo.Outgoings;
+import com.kh.ITWorks.approval.model.vo.OutgoingsList;
+import com.kh.ITWorks.approval.model.vo.Referer;
+
 public class ApprovalDao {
+
+	public int insertApprovalDocument(ApprovalDocument ad, SqlSessionTemplate sqlSession) {
+		return 0;
+	}
+
+	public int insertApprovalAttachment(Attachment a, SqlSessionTemplate sqlSession) {
+		return 0;
+	}
+
+	public int insertApprovalLine(ApprovalLine al, SqlSessionTemplate sqlSession) {
+		return 0;
+	}
+
+	public int insertApprovalRefere(Referer r, SqlSessionTemplate sqlSession) {
+		return 0;
+	}
+
+	public int insertOutgoings(Outgoings o, SqlSessionTemplate sqlSession) {
+		return 0;
+	}
+
+	public int insertOutgoingsList(ArrayList<OutgoingsList> oList, SqlSessionTemplate sqlSession) {
+		return 0;
+	}
+
+	public int insertDocumentsRequest(ArrayList<DocumentsRequest> drList, SqlSessionTemplate sqlSession) {
+		return 0;
+	}
 
 }
